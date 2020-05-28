6b5f8868ae11294a799bc955592055ab9baccc3a
nukeeper <david.cook@bglgroup.co.uk>
2020-05-28 15:37:04 +0100

    Added slides - first draft

--- PropertyBasedTesting/ManuallyCreatedPropertyBasedTests.cs
+++ PropertyBasedTesting/ManuallyCreatedPropertyBasedTests.cs
@@ -8,6 +8,20 @@ namespace PropertyBasedTesting
    public class ManuallyCreatedPropertyBasedTests
    {
       [Test]
+      public void ShouldAddRandomNumbersTogether()
+      {
+         var rand = new Random();
+         var a = rand.Next();
+         var b = rand.Next();
+      
+         var result = a.Add(b);
+      
+         var expected = a + b;
+      
+         Assert.That(result, Is.EqualTo(expected));
+      }
+      
+      [Test]
       public void Order_Of_Params_Does_Not_Matter()
       {
          for (var i = 0; i < 100; i++)
@@ -15,15 +29,15 @@ namespace PropertyBasedTesting
             var rand = new Random();
             var a = rand.Next();
             var b = rand.Next();
-
+      
             var result = a.Add(b);
-
+      
             var expected = b.Add(a);
-
+      
             Assert.That(result, Is.EqualTo(expected));
          }
       }
-
+      
       [Test]
       public void Adding_1_Twice_Is_The_Same_As_Adding_2_Once()
       {
@@ -31,15 +45,15 @@ namespace PropertyBasedTesting
          {
             var rand = new Random();
             var x = rand.Next();
-
+      
             var result = x.Add(1).Add(1);
-
+      
             var expected = x.Add(2);
-
+      
             Assert.That(result, Is.EqualTo(expected));
          }
       }
-
+      
       [Test]
       public void Adding_Zero_To_X_Returns_X()
       {
@@ -47,9 +61,9 @@ namespace PropertyBasedTesting
          {
             var rand = new Random();
             var x = rand.Next();
-
+      
             var result = x.Add(0);
-
+      
             Assert.That(result, Is.EqualTo(x));
          }
       }
