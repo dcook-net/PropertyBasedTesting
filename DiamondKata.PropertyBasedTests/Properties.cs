a7a9b20c1d431b313e95061a81dd2320d2246e17
nukeeper <david.cook@bglgroup.co.uk>
2020-05-31 19:15:51 +0100

    Add property tests

--- DiamondKata.PropertyBasedTests/Properties.cs
+++ DiamondKata.PropertyBasedTests/Properties.cs
@@ -1,19 +1,93 @@
+using System.Collections.Generic;
+using System.Linq;
 using FsCheck;
 using Xunit;
 using FsCheck.Xunit;
-using DiamondKata;
 
 namespace DiamondKata.PropertyBasedTests
 {
     public class Properties
     {
-        /// Already setup to use a custom Generator of Valid Characters: a-z or A-Z
-        [Property(Verbose = true, Arbitrary=new [] {typeof(ValidCharacters)})]
-        // [Property(Verbose = true, Arbitrary=new [] {typeof(InValidCharacters)})]
-        public void OurFirstProperty(char letter)
+        private List<(char letter, int index, int lettersPerRow)> _expectations;
+        
+        public Properties()
         {
-            //TODO: write our first Property Based Test
+            _expectations = Init();
         }
+
+        [Property(Verbose = true, Arbitrary = new[] {typeof(InValidCharacters)})]
+        public void ShouldReturnEmptyCollectionForNonLetterInputs(char letter)
+        {
+            Assert.Equal(new List<string>(), Diamond.Create(letter));
+        }
+
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void ResultShouldContainExpectedNumberOfRows(char letter)
+        {
+            var result = Diamond.Create(letter);
+
+            var expectedNumberOfRows = _expectations.Find(x => x.letter.ToString() == letter.ToString().ToUpper()).index;
+
+            Assert.Equal(expectedNumberOfRows, result.Count());
+        }
+
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void FirstAndLastRowsShouldContainASingleA(char letter)
+        {
+            var result = Diamond.Create(letter).ToList();
+
+            Assert.Equal("A", result.First().Trim());
+            Assert.Equal("A", result.Last().Trim());
+        }
+        //
+        // [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        // public void NumberOfRowsShouldEqualCharsPerRow(char letter)
+        // {
+        //     var result = Diamond.Create(letter).ToList();
+        //
+        //     var numbersOfRows = result.Count;
+        //
+        //     foreach (var row in result)
+        //     {
+        //         Assert.Equal(row.Length, numbersOfRows);
+        //     }
+        // }
+        //
+        // [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        // public void ShouldHaveTheCorrectNumberOfLettersPerRow(char letter)
+        // {
+        //     var result = Diamond.Create(letter).ToList();
+        //
+        //     var index = 0;
+        //     var last = result.Count - 1;
+        //     foreach (var row in result)
+        //     {
+        //         var expected = index == 0 || index == last ? 1 : 2;
+        //         Assert.Equal(row.Trim().ToCharArray().Length, expected);
+        //         index++;
+        //     }
+        // }
+
+        // [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        // public void NumberOfRowsShouldBeCorrect(char letter)
+        // {
+        //     var result = Diamond.Create(letter).ToList();
+        //
+        //     var index = 0;
+        //     foreach (var row in result)
+        //     {
+        //         var (c, i, lettersPerRow) = _expectations[index];
+        //
+        //         var letters = row.Trim().ToCharArray();
+        //         Assert.Equal(letters.Length, lettersPerRow);
+        //         foreach (var l in letters)
+        //         {
+        //             Assert.Equal(l, c);
+        //         }
+        //         
+        //         index++;
+        //     }
+        // }
 
 // Expected output for F:        
 //     A     
@@ -27,7 +101,12 @@ namespace DiamondKata.PropertyBasedTests
 //   C   C   
 //    B B    
 //     A    
-        
+
+        private static List<(char letter, int index, int lettersPerRow)> Init()
+        {
+            return Enumerable.Range(1, 26).Select(x => ((char)(x + 64), x, x == 1 ? 1 : 2)).ToList();
+        }
+
         private class ValidCharacters
         {
             public static Arbitrary<char> Char()
--- DiamondKata/Properties.cs
+++ DiamondKata/Properties.cs
@@ -1,7 +1,30 @@
-﻿namespace DiamondKata
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DiamondKata
 {
     public class Diamond
     {
-        //TODO: Add Implementation
+        public static IEnumerable<string> Create(char letter)
+        {
+            if (!char.IsLetter(letter))
+                return new List<string>();
+
+            var count = letter.ToInt();
+
+            var result = new List<string>(Enumerable
+                .Range(1, count)
+                .Select(x => x == 1 || x == count ? "A" : " "));
+            
+            return result;
+        }
+    }
+    
+    public static class CharExtensions
+    {
+        public static int ToInt(this char letter)
+        {
+            return letter.ToString().ToUpper().ToCharArray().First() - 64;
+        }
     }
 }
