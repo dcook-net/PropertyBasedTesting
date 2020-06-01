44cdd6a1f73f9ce65228cc165c566f5d84c22f45
nukeeper <david.cook@bglgroup.co.uk>
2020-06-01 16:03:59 +0100

    Kata Complete

--- DiamondKata.PropertyBasedTests/Properties.cs
+++ DiamondKata.PropertyBasedTests/Properties.cs
@@ -8,7 +8,7 @@ namespace DiamondKata.PropertyBasedTests
 {
     public class Properties
     {
-        private List<(char letter, int index, int lettersPerRow)> _expectations;
+        private readonly List<(char Letter, int LettersPerRow, int ExpectedNumberOfRows)> _expectations;
         
         public Properties()
         {
@@ -20,15 +20,15 @@ namespace DiamondKata.PropertyBasedTests
         {
             Assert.Equal(new List<string>(), Diamond.Create(letter));
         }
-
+        
         [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
         public void ResultShouldContainExpectedNumberOfRows(char letter)
         {
             var result = Diamond.Create(letter);
 
-            var expectedNumberOfRows = _expectations.Find(x => x.letter.ToString() == letter.ToString().ToUpper()).index;
+            var expectations = GetExpectationsForLetter(letter);
 
-            Assert.Equal(expectedNumberOfRows, result.Count());
+            Assert.Equal(expectations.expectedNumberOfRows, result.Count());
         }
 
         [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
@@ -39,72 +39,94 @@ namespace DiamondKata.PropertyBasedTests
             Assert.Equal("A", result.First().Trim());
             Assert.Equal("A", result.Last().Trim());
         }
-        //
-        // [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
-        // public void NumberOfRowsShouldEqualCharsPerRow(char letter)
-        // {
-        //     var result = Diamond.Create(letter).ToList();
-        //
-        //     var numbersOfRows = result.Count;
-        //
-        //     foreach (var row in result)
-        //     {
-        //         Assert.Equal(row.Length, numbersOfRows);
-        //     }
-        // }
-        //
-        // [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
-        // public void ShouldHaveTheCorrectNumberOfLettersPerRow(char letter)
-        // {
-        //     var result = Diamond.Create(letter).ToList();
-        //
-        //     var index = 0;
-        //     var last = result.Count - 1;
-        //     foreach (var row in result)
-        //     {
-        //         var expected = index == 0 || index == last ? 1 : 2;
-        //         Assert.Equal(row.Trim().ToCharArray().Length, expected);
-        //         index++;
-        //     }
-        // }
-
-        // [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
-        // public void NumberOfRowsShouldBeCorrect(char letter)
-        // {
-        //     var result = Diamond.Create(letter).ToList();
-        //
-        //     var index = 0;
-        //     foreach (var row in result)
-        //     {
-        //         var (c, i, lettersPerRow) = _expectations[index];
-        //
-        //         var letters = row.Trim().ToCharArray();
-        //         Assert.Equal(letters.Length, lettersPerRow);
-        //         foreach (var l in letters)
-        //         {
-        //             Assert.Equal(l, c);
-        //         }
-        //         
-        //         index++;
-        //     }
-        // }
-
-// Expected output for F:        
-//     A     
-//    B B    
-//   C   C   
-//  D     D  
-// E       E 
-//F         F
-// E       E 
-//  D     D  
-//   C   C   
-//    B B    
-//     A    
-
-        private static List<(char letter, int index, int lettersPerRow)> Init()
+ 
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void NumberOfRowsShouldEqualCharsPerRow(char letter)
         {
-            return Enumerable.Range(1, 26).Select(x => ((char)(x + 64), x, x == 1 ? 1 : 2)).ToList();
+            var result = Diamond.Create(letter).ToList();
+        
+            var numbersOfRows = result.Count;
+        
+            foreach (var row in result)
+            {
+                Assert.Equal(numbersOfRows, row.Length);
+            }
+        }
+
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void ShouldBeHorizontallySymmetrical(char letter)
+        {
+            var result = Diamond.Create(letter).ToList();
+
+            foreach (var row in result)
+            {
+                var (left, right) = row.SplitInTwo();
+
+                Assert.Equal(left, right);
+            }
+        }
+
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void ShouldHaveTheCorrectNumberOfLettersPerRow(char letter)
+        {
+            var result = Diamond.Create(letter).ToList();
+
+            foreach (var row in result)
+            {
+                var chars = row.Trim().ToCharArray().Where(char.IsLetter).ToList();
+                var expectations = GetExpectationsForLetter(chars.First());
+                Assert.Equal(expectations.lettersPerRow, chars.Count);
+            }
+        }
+
+        //This didn't force us to add any behaviour!
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void ResultShouldBeHorizontallySymmetrical(char letter)
+        {
+            var results = Diamond.Create(letter).ToList();
+            var resultsFlippedHorizontally = results;
+            resultsFlippedHorizontally.Reverse();
+
+            for (var i = 0; i < results.Count; i++)
+            {
+                Assert.Equal(results[i], resultsFlippedHorizontally[i]);
+            }
+        }
+
+        [Property(Verbose = true, Arbitrary = new[] {typeof(ValidCharacters)})]
+        public void ShouldFormAPoint(char letter)
+        {
+            var results = Diamond.Create(letter).ToList();
+
+            var midPoint = letter.ToInt() - 1;
+
+            var expectedNumberOfLeadingSpaces = 0;
+            for (var i = midPoint; i >= 0; i--)
+            {
+                var row = results[i];
+                var c = (i+1).ToChar();
+                var actualLeadingSpaces = row.Substring(0, row.IndexOf(c));
+                var reversedRow = row.Reverse().Aggregate("", (a,b) => a+b);
+                var actualTrailingSpaces = reversedRow.Substring(0, reversedRow.IndexOf(c));
+                
+                Assert.Equal(expectedNumberOfLeadingSpaces, actualLeadingSpaces.Length);
+                Assert.Equal(expectedNumberOfLeadingSpaces, actualTrailingSpaces.Length);
+
+                expectedNumberOfLeadingSpaces++;
+            }
+        }
+
+        private (char letter, int lettersPerRow, int expectedNumberOfRows) GetExpectationsForLetter(char letter)
+        {
+            return _expectations
+                .First(x => x.Letter.ToString() == letter.ToString().ToUpper());
+        }
+        
+        private static List<(char Letter, int LettersPerRow, int ExpectedNumberOfRows)> Init()
+        {
+            return Enumerable
+                .Range(1, 26)
+                .Select(x => ((char)(x + 64), x == 1 ? 1 : 2, x*2-1)).ToList();
         }
 
         private class ValidCharacters
@@ -123,6 +145,16 @@ namespace DiamondKata.PropertyBasedTests
                 return Arb.Default.Char()
                     .Filter(c => c < 65 || c >= 123 || c > 90 && c < 97);
             }
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static (string left, string right) SplitInTwo(this string row)
+        {
+            var left = row.Substring(0, row.Length / 2 + 1);
+            var right = row.Substring(row.Length / 2).ToCharArray().Reverse().Aggregate("", (a, b) => a.ToString() + b.ToString());
+            return (left, right);
         }
     }
 }
--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DiamondKata
@@ -10,13 +11,65 @@ namespace DiamondKata
             if (!char.IsLetter(letter))
                 return new List<string>();
 
-            var count = letter.ToInt();
+            var top = BuildTop(letter);
 
-            var result = new List<string>(Enumerable
-                .Range(1, count)
-                .Select(x => x == 1 || x == count ? "A" : " "));
+            return AddBottom(top);;
+        }
+
+        private static List<string> BuildTop(char letter)
+        {
+            var numberOfLetters = letter.ToInt();
+            var rowLength = numberOfLetters * 2 - 1;
+            var innerBufferLength = 1;
+
+            Func<int> GetInnerBufferLength = () =>
+            {
+                var length = innerBufferLength;
+                innerBufferLength += 2;
+                return length;
+            };
+
+            return new List<string>(
+                Enumerable
+                    .Range(1, numberOfLetters)
+                    .Select(x => x == 1 ? "A" : AddInnerBuffer(x.ToChar(), GetInnerBufferLength()))
+                    .Select(x => AddOuterBuffer(x, x == "A" ? numberOfLetters - 1 : (rowLength - x.Length) / 2)));
+        }
+
+        private static IEnumerable<string> AddBottom(IReadOnlyCollection<string> top)
+        {
+            var bottom = top.Take(top.Count - 1).Reverse();
             
+            var result = new List<string>();
+            result.AddRange(top);
+            result.AddRange(bottom);
+
             return result;
+        }
+
+
+        private static string AddInnerBuffer(char letter, int lengthOfBuffer)
+        {
+            var buffer = BuildBuffer(lengthOfBuffer);
+            return $"{letter}{buffer}{letter}";
+        }
+
+        private static string AddOuterBuffer(string value, int length)
+        {
+            if (length <= 0) return value;
+            
+            var buffer = BuildBuffer(length);
+                
+            return $"{buffer}{value}{buffer}";
+        }
+
+        private static string BuildBuffer(int length)
+        {
+            var buffer = Enumerable
+                .Range(1, length)
+                .Select(x => " ")
+                .Aggregate((a, b) => a + b);
+            return buffer;
         }
     }
     
@@ -25,6 +78,14 @@ namespace DiamondKata
         public static int ToInt(this char letter)
         {
             return letter.ToString().ToUpper().ToCharArray().First() - 64;
+        }
+    }
+
+    public static class IntExtensions
+    {
+        public static char ToChar(this int number)
+        {
+            return (char)(number + 64);
         }
     }
 }
