6ecf504568f5e1d8443c1d2adafff059e0b8ee60
MichaelTempest <michael.tempest@bglgroup.co.uk>
2020-06-22 14:59:58 +0100

    MOB - well this is where we got to

--- DiamondKata.PropertyBasedTests/Properties.cs
+++ DiamondKata.PropertyBasedTests/Properties.cs
@@ -1,18 +1,64 @@
 using FsCheck;
+using System;
 using Xunit;
 using FsCheck.Xunit;
 using DiamondKata;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace DiamondKata.PropertyBasedTests
 {
     public class Properties
     {
         /// Already setup to use a custom Generator of Valid Characters: a-z or A-Z
-        [Property(Verbose = true, Arbitrary=new [] {typeof(ValidCharacters)})]
-        // [Property(Verbose = true, Arbitrary=new [] {typeof(InValidCharacters)})]
-        public void OurFirstProperty(char letter)
+        // [Property(Verbose = true, Arbitrary=new [] {typeof(ValidCharacters)})]
+        [Property(Verbose = true, Arbitrary=new [] {typeof(InValidCharacters)})]
+        public void InvalidCharacters_Should_ReturnEmptyResponse(char letter)
         {
-            //TODO: write our first Property Based Test
+            Diamond diamond = new Diamond();
+            var diamondCollection = diamond.ConvertToDiamond(letter);
+
+            Assert.Equal(Enumerable.Empty<string>(), diamondCollection);
+        }
+
+        [Property(Verbose = true, Arbitrary=new [] {typeof(ValidCharacters)})]
+        public void ValidCharacters_Should_HaveOddNumberOfRows(char letter)
+        {
+            Diamond diamond = new Diamond();
+            var diamondCollection = diamond.ConvertToDiamond(letter);
+
+            Assert.True(diamondCollection.Count() % 2 == 1);
+        }
+
+        [Property(Verbose = true, Arbitrary=new [] {typeof(ValidCharacters)})]
+        public void ValidCharacters_Should_ResultInDiamondOfExpectedHeight(char letter)
+        {
+            var upperChar = char.ToUpper(letter);
+            var letterIndex = (upperChar - 64);
+            var expected = letterIndex * 2 - 1;
+            Diamond diamond = new Diamond();
+
+            var diamondCollection = diamond.ConvertToDiamond(upperChar);
+
+            Assert.Equal(expected, diamondCollection.Count());
+        }
+
+        [Property(Verbose = true, Arbitrary=new [] {typeof(ValidCharacters)})]
+        // [Theory]
+        // [InlineData('B')]
+        public void ValidCharacters_Should_ResultInEachRowBeingSameLengthAsDiamondHeight(char letter)
+        {
+            var letterIndex = (letter - 64);
+            var expected = letterIndex * 2 - 1;
+            Diamond diamond = new Diamond();
+
+            var diamondCollection = diamond.ConvertToDiamond(letter);
+
+            Console.WriteLine("Look at this");
+            Console.WriteLine(expected);
+            Console.WriteLine(diamondCollection.First().Count());
+
+            Assert.True(diamondCollection.All(x => x.Count() == expected));
         }
 
 // Expected output for F:        
--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -1,7 +1,39 @@
-﻿namespace DiamondKata
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DiamondKata
 {
     public class Diamond
     {
-        //TODO: Add Implementation
+        private readonly int initialLetter = 65;
+
+        public IEnumerable<string> ConvertToDiamond(char letter)
+        {
+            if(!char.IsLetter(letter))
+                return new List<string>();
+
+            var upperChar = char.ToUpper(letter);
+            var diamondList = new List<string>();
+            var extraSpacing = 2;
+            var lengthBetweenFirstAndSecondLetter = upperChar - initialLetter + extraSpacing;
+
+            var range = Enumerable.Range(initialLetter, lengthBetweenFirstAndSecondLetter);
+            var content = range
+                .Select(x => ".")
+                .Aggregate((x, y) => x + y);
+
+            for (int i = initialLetter; i <= upperChar; i++)
+            {
+                diamondList.Add(content);
+            }
+
+            for (int i = upperChar - 1; i >= initialLetter; i--)
+            {
+                diamondList.Add(content);
+            }
+
+            return diamondList;
+        }
     }
 }
